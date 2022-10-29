4afd00a926b6fe9ee0c91a3637ab16e2c8d95d36
shamil-k <bareabhilash2000@gmail.com>
2022-10-29 15:20:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("")
+  const handleEvent = (event) => {
+    setInput(event.target.value);
+
+  }
+  const sum = () => {
+    alert("clicked me ")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>sgadf</h1>
+      <input type="text" onChange={handleEvent} name="name" placeholder='Enter your name' />
+      <button onClick={sum}>Click me</button>
+      <br></br>
+      {input}
+    </>
   );
 }
 
